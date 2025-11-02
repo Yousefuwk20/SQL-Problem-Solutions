e51da4465185dc7e39a953725deb4669483616e8
Yousef Mohamed <yousef.zsc123@gmail.com>
2025-11-02 20:20:06 +0200

    Probelm solution

---
+++ datalemur/Average Post Hiatus (Part 1).sql
@@ -0,0 +1,18 @@
+-- Problem link: https://datalemur.com/questions/sql-average-post-hiatus-1
+-- Problem Difficulty: Easy
+WITH diff_date AS(
+SELECT 
+  user_id,
+  MIN(post_date) AS first_post_date,
+  MAX(post_date) AS last_post_date
+FROM posts
+WHERE EXTRACT(YEAR FROM post_date) = 2021
+GROUP BY user_id
+)
+
+SELECT 
+      user_id,
+      (last_post_date::date - first_post_date::date) AS days_between
+FROM diff_date
+WHERE (last_post_date::date - first_post_date::date) > 0
+
