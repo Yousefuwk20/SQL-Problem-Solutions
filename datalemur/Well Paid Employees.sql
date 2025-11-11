c788c3a464d736f7a80fe2824cf92dfd45cab9b1
Yousef Mohamed <yousef.zsc123@gmail.com>
2025-11-11 19:30:43 +0200

    Problem Solution

---
+++ datalemur/Well Paid Employees.sql
@@ -0,0 +1,16 @@
+-- Problem link: https://datalemur.com/questions/sql-well-paid-employees
+-- Problem Difficulty: Easy
+
+WITH managers AS(
+SELECT * 
+FROM employee
+WHERE manager_id IS NULL
+)
+
+SELECT 
+    e.employee_id,
+    e.name AS employee_name
+FROM employee e 
+INNER JOIN managers m 
+ON e.manager_id = m.employee_id
+WHERE e.salary > m.salary
