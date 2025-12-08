9e4c6cbc692e79cf29331a0890a94d092dab759d
Yousef Mohamed <yousef.zsc123@gmail.com>
2025-12-08 03:04:28 +0200

    Problem Solution

---
+++ datalemur/IBM db2 Product Analytics.sql
@@ -0,0 +1,21 @@
+-- Problem link: https://datalemur.com/questions/sql-ibm-db2-product-analytics
+-- Problem Difficulty: Easy
+
+WITH emp AS(
+SELECT 
+    e.employee_id,
+    COUNT(q.query_id) AS num_of_queries
+FROM employees e
+LEFT JOIN queries AS q
+ON e.employee_id = q.employee_id
+AND q.query_starttime >= '2023-07-01T00:00:00Z'
+AND q.query_starttime < '2023-10-01T00:00:00Z'
+GROUP BY e.employee_id
+)
+
+SELECT
+    num_of_queries,
+    COUNT(employee_id)
+FROM emp
+GROUP BY num_of_queries
+ORDER BY num_of_queries;
