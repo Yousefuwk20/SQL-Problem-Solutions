d8cb7dfb51eeb9369f736cf460aa5db240a790bb
Yousef Mohamed <yousef.zsc123@gmail.com>
2026-02-08 18:46:26 +0200

    New problem

---
+++ datalemur/medium/Best-Selling Product.sql
@@ -0,0 +1,28 @@
+-- Problem link: https://datalemur.com/questions/best-selling-products
+
+
+
+
+
+
+
+
+
+WITH rnk AS(
+  SELECT 
+    RANK() OVER(
+      PARTITION BY p.category_name 
+      ORDER BY ps.sales_quantity DESC, ps.rating DESC) AS rating_rnk,
+    p.product_name AS product_name,
+    p.category_name AS category_name
+  FROM products p 
+  INNER JOIN product_sales ps
+  ON p.product_id = ps.product_id
+  ORDER BY p.category_name, p.product_name
+)
+
+SELECT 
+  category_name,
+  product_name
+FROM rnk
+WHERE rating_rnk = 1
