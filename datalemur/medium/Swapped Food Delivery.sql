a20640d303c596b625478f92afca619b3b6ec4ff
Yousef Mohamed <yousef.zsc123@gmail.com>
2026-02-13 17:05:44 +0200

    Levi

--- datalemur/medium/Swapped Food Delivery.sql
+++ datalemur/medium/Swapped Food Delivery.sql
@@ -5,7 +5,7 @@ SELECT
   CASE 
     WHEN (order_id % 2 = 1 AND COUNT(*) OVER() = order_id) THEN order_id
     WHEN order_id % 2 = 0 THEN order_id - 1
-    WHEN order_id % 2 = 1 THEN order_id + 1
+    ELSE order_id + 1
   END corrected_order_id
 FROM orders
 ORDER BY corrected_order_id
